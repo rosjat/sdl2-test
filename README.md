6ae12d7d72cf81aa4066073435b9d4cd9d1dda65
Markus rosjat <markus.rosjat@gmail.com>
2021-03-18 14:08:02 +0100

    adding a little more action
    loading a background image and switch stages

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ simple sdl2 project
 
 its basically a playground for myself to get used to some things in C and have at least some vidual suggar :)
 
+good place to start with sdl2: https://wiki.libsdl.org/
+
+i shamelessly stole a background from https://spritedatabase.net
+
+
+
 
--- hello.c
+++ hello.c
@@ -12,20 +12,18 @@
 
 #define SCROLL_SPEED  (300)
 
-typedef struct {
-    int left;
-    int right;
-    int up;
-    int down;
 
-} player;
 int main(){
 
     
     SDL_Texture* player;
-    SDL_Texture* brick;
+    SDL_Texture* bg;
 
-    /* Initialize SDL’s video system and check for errors */
+    /* Initialize Start 
+
+        everything in this block is needed to get the SDL stuff going.
+        And of course we do a minimal error handling.
+    */
     if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER) != 0) {
         printf("Unable to initialize SDL: %s\n", SDL_GetError());
         return 1;
@@ -50,7 +48,9 @@ int main(){
         SDL_Quit();
         return 1;
     }
+    /* Initialze End */
 
+    /* getting a container for some bitmaps we want to load and apply it  textures */
     SDL_Surface* surface = IMG_Load("img/test.png");
     if(!surface) {
         printf("Unable to create Surface: %s\n", SDL_GetError());
@@ -60,32 +60,48 @@ int main(){
         return 1;
     }
 
+    /* adding bitmaps to textures */ 
     player = SDL_CreateTextureFromSurface(renderer, surface);
-    surface = IMG_Load("img/test2.png");
-    brick = SDL_CreateTextureFromSurface(renderer, surface);
+    surface = IMG_Load("img/bg.png");
+    bg = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
-    if(!player || !brick) {
+    if(!player || !bg) {
         printf("Unable to create Texture: %s\n", SDL_GetError());
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
         return 1;
     }
+
+    /* get the size of the texture but since this will be the rect to display only 
+       a single sprite of the whole sheet we scale it down to the size of one sprite
+       
+       our sprite sheet has 4 animations with 3 frames each.
+       first row is runing down
+       second row is running left
+       third row is running right 
+       fourth row is running up
+
+       dest is acting as the object to be draged around while the other rect (later on) 
+       is acting and a second rect will copy one specific sprite onto the dest rect.
+
+       Note: this is just helpful for myself no one else :)
+    */
     SDL_Rect dest;
-
+    SDL_Rect bg_rect;
     SDL_QueryTexture(player, NULL, NULL, &dest.w, &dest.h);
-
-    // scale the image down to a quater of his original size
-    //player_rect.x = player_rect.y = 0;
+    SDL_QueryTexture(bg, NULL, NULL, &bg_rect.w, &bg_rect.h);
     dest.w /= 3;
     dest.h /= 4;
 
+    // setting up inital positions and velocities
     float x_pos = (WINDOW_WIDTH - dest.w) / 2;
     float y_pos = (WINDOW_HEIGHT - dest.h) / 2;
 
     float x_velo = 0;
     float y_velo = 0;
 
+    // helper for keyboard input and navigation
     int up = 0;
     int down = 0;
     int left = 0;
@@ -93,8 +109,13 @@ int main(){
 
     int running = 1;
     
+    int stage = 0;
+    int bg_offset = (250 * 3);
+
+    // the so called game loop, i guess we know what it does
     while(running) {
 
+        // setting up event handing for the window and keyboard
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
             switch(event.type) {
@@ -155,32 +176,28 @@ int main(){
             }
         }
         
-        //determine velocity
         x_velo = y_velo = 0;
-        // do some mouse stuff
+        
+        // doing some mouse actions, this might be the point to figure out how keyboard and
+        // mouse play nice together ...
+        
         int mouse_x, mouse_y;
         int buttons = SDL_GetMouseState(&mouse_x, &mouse_y);
-
         int target_x = mouse_x - dest.w / 2;
         int target_y = mouse_y - dest.h / 2;
         float delta_x = target_x - x_pos;
         float delta_y = target_y -y_pos;
         float distance = sqrt(delta_x * delta_x + delta_y * delta_y);
 
+        // we do the sprite selection based on a few time related things so we see the little 
+        // guy moving in a sane way  ....
         SDL_Rect player_sprite;
-
-         int totalFrames = 3;
-
-        //Amount of delay in milliseconds for each frame
+        int totalFrames = 3;
         int delayPerFrame = 100;
-
-        //SDL_GetTicks() method gives us the time in milliseconds
-
-        //'frame' will give us the index of frame we want to render
-        //For example, if 'frame' is 2, it will give us the third frame
         int frame = (SDL_GetTicks() / delayPerFrame) % totalFrames;
         player_sprite.x = frame * 32;
-
+        // move in the right direction ? This relates to mouse input and is shakey  at best
+        // for keyboard we need to rething a little 
         if ((delta_y < -5) &&(-5 > delta_x < 5))
             player_sprite.y = 96;
         if ((delta_y > 5) &&(-5 > delta_x < 5))
@@ -194,6 +211,7 @@ int main(){
         player_sprite.h = dest.h;
         player_sprite.w = dest.w;
 
+        // prevent jitter for now
         if(distance < 5) {
             player_sprite.x = 0;
             x_velo = y_velo = 0;
@@ -201,10 +219,12 @@ int main(){
             x_velo = delta_x * SCROLL_SPEED / distance;
             y_velo = delta_y * SCROLL_SPEED / distance;
         }
+        // just make the mouse chase the sprite or the other way round 
         if(buttons & SDL_BUTTON(SDL_BUTTON_LEFT)) {
             x_velo = -x_velo;
             y_velo = -y_velo;
         }
+
         if(up && !down)
             y_velo = -SCROLL_SPEED;
         if(down && !up)
@@ -217,18 +237,43 @@ int main(){
         x_pos += x_velo / 60;
         y_pos += y_velo / 60;
         
-        //collition detection with the window bounds
+        // setting up the background so we can change it when going up and down
+        SDL_Rect bg_part;
+        bg_part.w = WINDOW_WIDTH;
+        bg_part.h = WINDOW_HEIGHT;
+        bg_part.x = 0;
+        bg_part.y = 0;
+        bg_rect.w = 245;
+        bg_rect.h = 225;
+        bg_rect.x = 265 * 2;
+        bg_rect.y = bg_offset + (bg_rect.h * stage);
+
+        //collition detection with the window bounds and setting the bg
         if(x_pos <= 0) {
             x_pos = 0;
         }
         if(y_pos <= 0) {
-            y_pos = 0;
+            if(stage > 0)
+            {
+                stage--;
+                y_pos = 0 + dest.h;
+            }
+            else {
+                y_pos = 0;
+            }
         }
         if(x_pos >= WINDOW_WIDTH - dest.w) {
             x_pos = WINDOW_WIDTH - dest.w;
         }
         if(y_pos >= WINDOW_HEIGHT - dest.h) {
-            y_pos = WINDOW_HEIGHT - dest.h;
+            if(stage < 2)
+            {
+                stage++;
+                y_pos = 0 + dest.h;
+            }
+            else {
+                y_pos = WINDOW_HEIGHT - dest.h;
+            }
         }
 
 
@@ -236,10 +281,10 @@ int main(){
         dest.y = (int) y_pos;
         
         SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, bg, &bg_rect, &bg_part);
         SDL_RenderCopy(renderer, player, &player_sprite, &dest);
         SDL_RenderPresent(renderer);
     }
-
 
     SDL_DestroyTexture(player);
     SDL_DestroyRenderer(renderer);
